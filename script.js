5c07032f498e48aebc92cd0f5961d3ff3b282048
isaque911 <isaquedecarvalho1756@gmail.com>
2025-08-22 14:35:25 -0300

    Create script.js
    Adicionando o cérebro da calculadora

---
+++ script.js
@@ -0,0 +1,23 @@
+function soma() { 
+  let p = prompt('Digite um número:') 
+  let p2 = prompt('Digite outro número:') 
+  return alert(parseFloat(p)+parseFloat(p2)) 
+} 
+
+function subtrair() { 
+  let pergunta = prompt('Digite um número:') 
+  let pergunta2 = prompt('Digite outro número:') 
+  return alert(pergunta-pergunta2) 
+} 
+
+function multiplicar() {
+  let pergunta = prompt('Digite um número:')
+  let pergunta2 = prompt('Digite outro número:') 
+  return alert(pergunta*pergunta2)
+}
+
+function dividir() {
+  let pergunta = prompt('Digite um número:') 
+  let pergunta2 = prompt('Digite outro número:')
+  return alert(pergunta/pergunta2) 
+}
